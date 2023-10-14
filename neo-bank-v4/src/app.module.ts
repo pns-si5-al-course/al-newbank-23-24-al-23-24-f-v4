e1fd3aabd84770afc02c2c4cd94dcc1e3dd423d5
Cloud84-blip <64501479+Cloud84-blip@users.noreply.github.com>
2023-10-14 19:09:30 +0200

    added db and docker compose

--- neo-bank-v4/src/app.module.ts
+++ neo-bank-v4/src/app.module.ts
@@ -1,11 +1,14 @@
+import { DbTransactionModule } from './dbTransction/dbTransaction.module';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import configuration from 'shared/config/configuration';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RateModule } from './rate/rate.module';
+
 @Module({
   imports: [
+    DbTransactionModule,
     ConfigModule.forRoot({
       load: [configuration],
       isGlobal: true,
