493a9646429dbf8f0df0b2f13de121281c5cc410
Sylcantor <guiblinnicolas@gmail.com>
2023-10-22 14:42:59 +0200

    updated rates

--- backend_transaction/app/services/rates.py
+++ backend_transaction/app/services/rates.py
@@ -2,14 +2,16 @@ import aiohttp
 from config import FIXER_API_KEY
 
 
-async def get_rates():
-    print(FIXER_API_KEY)
-    url = f"http://data.fixer.io/api/latest?access_key={FIXER_API_KEY}"
-
-    print(url)
-
+async def get_rates(base: str, symbols: str):
+    
+    if symbols is None:
+        url = f"http://data.fixer.io/api/latest?access_key={FIXER_API_KEY}&base={base}"
+    else:
+        url = f"http://data.fixer.io/api/latest?access_key={FIXER_API_KEY}&base={base}&symbols={symbols}"
+    
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             data = await response.json()
-            print(data)
+            if not data.get('success'):
+                raise Exception("Failed to fetch the exchange rates.")
             return data
