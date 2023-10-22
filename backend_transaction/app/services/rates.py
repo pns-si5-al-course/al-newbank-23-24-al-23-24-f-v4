8fe669ca5786590d278bea9598f19fcab1bf35b5
Sylcantor <guiblinnicolas@gmail.com>
2023-10-22 19:03:44 +0200

    update rates

--- backend_transaction/app/services/rates.py
+++ backend_transaction/app/services/rates.py
@@ -11,23 +11,28 @@ async def get_rates(base: str, symbols: str):
     
     cache_key = f"{base}_{symbols}"
     
-    # Check if rates are in cache and not expired
-    if cache_key in CACHE and time.time() - CACHE[cache_key]['timestamp'] < CACHE_EXPIRATION:
-        #print("Fetching rates from cache !")
-        return CACHE[cache_key]['rates']
+    try:
+        # If not in cache or expired, fetch new rates
+        url = f"http://data.fixer.io/api/latest?access_key={FIXER_API_KEY}&base={base}&symbols={symbols}"
+        
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                data = await response.json()
+                if not data.get('success'):
+                    raise Exception("Failed to fetch the exchange rates.")
+                
+                # Update cache
+                CACHE[cache_key] = {
+                    'rates': data,
+                    'timestamp': time.time()
+                }
+                return data
     
-    # If not in cache or expired, fetch new rates
-    url = f"http://data.fixer.io/api/latest?access_key={FIXER_API_KEY}&base={base}&symbols={symbols}"
-    
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            data = await response.json()
-            if not data.get('success'):
-                raise Exception("Failed to fetch the exchange rates.")
-            
-            # Update cache
-            CACHE[cache_key] = {
-                'rates': data,
-                'timestamp': time.time()
-            }
-            return data
+    except Exception as e:
+        # If there is an error fetching the rates, check if they are in cache
+        if cache_key in CACHE:
+            print(f"Error fetching rates, using cached values: {e}")
+            return CACHE[cache_key]['rates']
+        else:
+            # If not in cache, re-raise the exception
+            raise Exception(f"Failed to fetch rates and no cached values available: {e}")
