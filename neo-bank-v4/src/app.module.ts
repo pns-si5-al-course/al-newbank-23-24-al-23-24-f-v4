a46175ceb99287ac7b9a642e1090e5fffc3d328b
Sylcantor <guiblinnicolas@gmail.com>
2023-10-24 10:27:31 +0200

    merge users-dev into backend_account

--- neo-bank-v4/src/app.module.ts
+++ neo-bank-v4/src/app.module.ts
@@ -1,21 +1,34 @@
 import { DbTransactionModule } from './dbTransction/dbTransaction.module';
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
-import configuration from 'shared/config/configuration';
+import configuration from 'config/configuration';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RateModule } from './rate/rate.module';
+
+import { UserModule } from './user/user.module';
+import mongodbConfig from 'shared/config/mongodb.configuration';
+import { MongooseConfigService } from 'shared/services/mongodb-configuration.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserController } from './user/controller/user.controller';
+import { UserService } from './user/service/user.service';
+
 
 @Module({
   imports: [
     DbTransactionModule,
     ConfigModule.forRoot({
-      load: [configuration],
+      load: [configuration, mongodbConfig],
       isGlobal: true,
     }),
-    RateModule,
+   //RateModule,
+    UserModule,
+    MongooseModule.forRootAsync({
+      useClass: MongooseConfigService,
+    })
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, UserController],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
