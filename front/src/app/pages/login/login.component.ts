f8d55ec9310a216d5d37dcba541a169be98868d8
Hadil Ayari <hadilayari00@gmail.com>
2023-11-07 12:05:13 +0100

    login + cors

--- front/src/app/pages/login/login.component.ts
+++ front/src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { LoginService } from 'src/services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  clientName: string = '';
+  accessCode: string = '';
+constructor (private loginService: LoginService){}
 
+loggingIn(){
+  this.loginService.logIn(this.clientName,this.accessCode);
+}
 }
