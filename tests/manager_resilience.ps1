b54d43ee1b3b9ab41ce5d8f2be75f49c8f951057
MOVSESSIAN <chahan.movsessian@sap.com>
2024-01-04 09:39:24 +0100

    added script for windows build and powershell test for resilience of manager

---
+++ tests/manager_resilience.ps1
@@ -0,0 +1,14 @@
+$startTime = Get-Date
+
+$duration = 2
+$index = 1
+
+do {
+    curl -X GET http://localhost:100/transaction_manager/
+    Write-Host "" $index
+    $index++
+    Start-Sleep -Seconds 2
+} while ((Get-Date) - $startTime -lt (New-TimeSpan -Minutes $duration))
+
+# Fin du script
+Write-Host "Exécution de curl terminée après 2 minutes."
