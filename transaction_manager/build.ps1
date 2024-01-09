1621ad09852bd459fbf3a61e2ac9ffdc180b2b2e
Cloud84-blip <64501479+Cloud84-blip@users.noreply.github.com>
2024-01-09 22:46:31 +0100

    fix powersl script and Architecture_diagram

--- transaction_manager/build.ps1
+++ transaction_manager/build.ps1
@@ -1,18 +1,21 @@
-# build docker image with file Dockerfile.production
+# Build docker image with file Dockerfile.production
 docker build -t transaction_manager -f Dockerfile .
 
 # Initiate sharding cluster
-## 1. Initiate config server
-docker compose -f sharding/config-server/docker-compose.yaml up -d
+# 1. Initiate config server
+docker-compose -f sharding/config-server/docker-compose.yaml up -d
 
-docker exec -i cfgsvr1 bash -c 'sh' < sharding/config-server/init-mongo.sh
+# Run init script on config server
+docker exec -i cfgsvr1 bash -c 'sh' | Get-Content sharding/config-server/init-mongo.sh
 
-## 2. Initiate shard1
+# 2. Initiate shard1
 docker-compose -f sharding/shard1/docker-compose.yaml up -d
 
-docker exec -i shard1svr1 bash -c 'sh' < sharding/shard1/init-mongo.sh
+# Run init script on shard1
+docker exec -i shard1svr1 bash -c 'sh' | Get-Content sharding/shard1/init-mongo.sh
 
-## 3. Initiate mongos
+# 3. Initiate mongos
 docker-compose -f sharding/mongos/docker-compose.yaml up -d
 
-docker exec -i mongos bash -c 'sh' < sharding/mongos/init-mongo.sh
+# Run init script on mongos
+docker exec -i mongos bash -c 'sh' | Get-Content sharding/mongos/init-mongo.sh
